dd23d12af189db2855d50eab5ae5af6a5a30bc43
Kushal Patel <kushal300603@gmail.com>
2024-01-01 06:04:25 +0530

    configured .gitignore and db.sql

---
+++ server/db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE restaurants(
+    restaurant_id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurant_name VARCHAR(60) NOT NULL,
+    restaurant_location VARCHAR(60) NOT NULL,
+    restaurant_price_range INT NOT NULL check(restaurant_price_range >= 1 and restaurant_price_range <= 5)
+);
+
+INSERT INTO restaurants (restaurant_id, restaurant_name, restaurant_location, restaurant_price_range)
+VALUES (123, 'rasna', 'surat', 4);
